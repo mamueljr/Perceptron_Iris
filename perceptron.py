4901096048ad39254d60eda002280aefe2f20d61
Emmanuel Rojas <mamueljr@gmail.com>
2023-04-24 18:10:20 -0600

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -4,24 +4,27 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
 # Cargar Iris
-iris=datasets.load_iris()
+iris = datasets.load_iris()
 
 # Features y labels
 x = iris.data
 y = iris.target
 # Split en training y test
-X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=0)
 # standarizar la data
 sc = StandardScaler()
-X_train_std=sc.fit_transform(X_train)
+X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.fit_transform(X_test)
 # Escoger el algoritmo para pasar los hiperparametros
-ppn = Perceptron(max_iter=1000, eta0= 0.0001, random_state=0, tol=0.001)
+ppn = Perceptron(max_iter=1000, eta0=0.0001, random_state=0, tol=0.001)
 # entrenar el modelo
-ppn.fit(X_train_std,y_train)
+ppn.fit(X_train_std, y_train)
 # Hacer la prediccion
 y_pred = ppn.predict(X_train_std)
 print('-----------')
-print((y_test != y_pred).sum(),'/',((y_test == y_pred).sum()+(y_test != y_pred).sum()))
-print('Accuracy: ',100*accuracy_score(y_test,y_pred))
+print((y_test != y_pred).sum(), '/',
+      ((y_test == y_pred).sum()+(y_test != y_pred).sum()))
+print('Accuracy: ', 100*accuracy_score(y_test, y_pred))
