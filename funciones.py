6db5dcfa115cbfcc346a62d0db6a08f0a64030b5
Emmanuel Rojas <mamueljr@gmail.com>
2023-04-27 19:56:54 -0600

    borrar inecesarios

--- funciones.py
+++
@@ -1,9 +0,0 @@
-import os
-import joblib
-
-
-def prediccion(data, model_name):
-    model_perceptron = joblib.load('model_perceptron.sav')
-    tr_data = joblib.load('pipeline_perceptron.sav')
-    tr_pipeline = tr_data.transform(data)
-    return model_perceptron.prediccion(tr_pipeline)
